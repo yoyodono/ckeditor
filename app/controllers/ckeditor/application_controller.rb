b13a8fd3452c355e370897361381d6993564f8a7
Yohanes Ekodono <yohanes.sustainablefish.org>
2022-07-13 18:40:58 +0800

    changed filter to action

--- app/controllers/ckeditor/application_controller.rb
+++ app/controllers/ckeditor/application_controller.rb
@@ -1,9 +1,9 @@
 class Ckeditor::ApplicationController < ApplicationController
   layout 'ckeditor/application'
 
-  before_filter :find_asset, :only => [:destroy]
-  before_filter :ckeditor_authorize!
-  before_filter :authorize_resource
+  before_action :find_asset, :only => [:destroy]
+  before_action :ckeditor_authorize!
+  before_action :authorize_resource
 
   protected
 
